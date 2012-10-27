a1f1d524b0d8130b95f761aee9325f95761688b5
matt-williams <matwilliams@hotmail.com>
2012-10-27 22:11:00 +0100

    Fix communication.

--- vHack Racer/src/com/github/matt/williams/vhack/racer/AccelerometerEventBroadcaster.java
+++ vHack Racer/src/com/github/matt/williams/vhack/racer/AccelerometerEventBroadcaster.java
@@ -5,69 +5,72 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
 public class AccelerometerEventBroadcaster implements ControllerCallback {
+    protected static final String TAG = AccelerometerEventBroadcaster.class.getName();
 
-	private Socket mEventSocket;
-	
-	public void start() {
-		(new SocketThread()).start();
-	}
+    private static final String SPEED = "speed";
+    private static final String STEERING = "steering";
+    private SocketThread socketThread;
 
-	@Override
-	public void control(float steering, float speed) {
-		(new BroadcastThread(steering, speed)).start();
-	}
+    public void start() {
+        socketThread = new SocketThread();
+        socketThread.start();
+    }
 
-	private class BroadcastThread extends Thread {
-		
-		private float mSteering;
-		private float mSpeed;
-		
-		public BroadcastThread(float steering, float speed) {
-			mSteering = steering;
-			mSpeed = speed;
-		}
+    public void control(float steering, float speed) {
+        Message msg = new Message();
+        Bundle bundle = new Bundle();
+        bundle.putFloat(STEERING, steering);
+        bundle.putFloat(SPEED, speed);
+        msg.setData(bundle);
+        socketThread.mHandler.sendMessage(msg);
+    }
 
-		public void run() {
-			PrintWriter out = null;
+    private class SocketThread extends Thread {
+        private Socket mEventSocket;
+        private PrintWriter mOut;
+        public Handler mHandler;
 
-			try {
-				out = new PrintWriter(mEventSocket.getOutputStream(), true);
+        public void run() {
+            Looper.prepare();
 
-				out.println(Float.toString(mSteering) + ", " + Float.toString(mSpeed));
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.err.println("I/O exception.");
-				System.exit(1);
-			} finally {
-				try {
-					if (out != null) {
-						out.close();
-					}
-					if (mEventSocket != null) {
-						mEventSocket.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+            mHandler = new Handler() {
+                public void handleMessage(Message msg) {
+                    Bundle bundle = msg.getData();
+                    Log.i(TAG, "Sending " + Float.toString(bundle.getFloat(STEERING)) + ", " + Float.toString(bundle.getFloat(SPEED)));
+                    mOut.println(Float.toString(bundle.getFloat(STEERING)) + ", " + Float.toString(bundle.getFloat(SPEED)));
+                }
+            };
+            
+            try {
+                mEventSocket = new Socket("192.168.1.68", 10569);
+                mOut = new PrintWriter(mEventSocket.getOutputStream(), true);
+            } catch (UnknownHostException e) {
+                System.err.println("Uknown host");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Couldn't get I/O for the connection.");
+            }
 
-	}
+            Looper.loop();
 
-	private class SocketThread extends Thread {
-
-		public void run() {
-			try {
-				mEventSocket = new Socket("192.168.1.68", 10569);
-			} catch (UnknownHostException e) {
-				System.err.println("Uknown host");
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.err.println("Couldn't get I/O for the connection.");
-			}
-		}
-
-	}
+            try {
+                if (mOut != null) {
+                    mOut.close();
+                }
+                if (mEventSocket != null) {
+                    mEventSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
